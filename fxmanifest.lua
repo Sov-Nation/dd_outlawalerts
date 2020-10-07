40ea24a02e752b740d0e4bfba0cebf3a33bbf946
DokaDoka <31368547+DokaDoka@users.noreply.github.com>
2020-10-07 19:37:23 +1100

    Update and rename __resource.lua to fxmanifest.lua
    to match boilerplate

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description "DokaDoka's Outlaw Alerts"
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'config.lua'
+}
